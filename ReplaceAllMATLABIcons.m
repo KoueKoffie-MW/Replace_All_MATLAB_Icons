61d8e29d28d05e975a516cb03bc73cfd28205b99
jjansev <jjansev@mathworks.com>
2024-11-18 16:01:10 +0100

    Added users location for shortcuts

--- ReplaceAllMATLABIcons.m
+++ ReplaceAllMATLABIcons.m
@@ -8,10 +8,14 @@
 
 function fileList = findMATLABShortcuts()
     
-        
+    [~, username] = system('echo %USERNAME%'); 
+    username = strtrim(username);
     % Define the root directory to search
     rootDir_1 = ('C:\ProgramData\Microsoft\Windows\Start Menu\Programs\');
     rootDir_2 = ('C:\Users\Public\Desktop');
+    rootDir_3 = char("C:\Users\" + username + "\AppData\Roaming\Microsoft\Internet Explorer\Quick Launch\User Pinned");
+
+    
 
     % Define the search pattern
     searchPattern = 'MATLAB ??????.lnk';
@@ -19,8 +23,9 @@ function fileList = findMATLABShortcuts()
     % Find all matching files
     fileList_1 = searchFilesRecursively(rootDir_1, searchPattern);
     fileList_2 = searchFilesRecursively(rootDir_2, searchPattern);
+    fileList_3 = searchFilesRecursively(rootDir_3, searchPattern);
 
-    fileList = [fileList_1 fileList_2];
+    fileList = [fileList_1 fileList_2 fileList_3];
 
     % Display the results
     if isempty(fileList)

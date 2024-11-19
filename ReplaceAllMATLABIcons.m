665545673d14db013188c76b97b222843724e350
jjansev <jjansev@mathworks.com>
2024-11-19 17:07:13 +0100

    Bug-Fixes for New-Desktop

--- ReplaceAllMATLABIcons.m
+++ ReplaceAllMATLABIcons.m
@@ -5,11 +5,13 @@
         else
             disp('MATLAB is running with administrative privileges. The script will continue');
         end
-
+clear result
 % Run the function
 Shortcuts = findMATLABShortcuts();
+
 flag = 0;
 Win_c = 1;
+
 for L = 1:length(Shortcuts)
     version_n = char(Shortcuts(L));
  
@@ -20,20 +22,19 @@ for L = 1:length(Shortcuts)
     version_new =  (version_new{end});
     version_new =  (version_new{end});
     version_new =  char(version_new{end,end});
-    
-    % version_new = version_n(end-6:end-4);
- version_new = strjoin(["matlab_", version_new, ".ico" ],'');
+    version_new = strjoin(["matlab_", version_new, ".ico" ],'');
 
- New_Icon = fullfile(pwd, 'matlab.icons',version_new);
+    New_Icon = fullfile(pwd, 'matlab.icons',version_new);
 
- % Create a COM server for the Windows Script Host Shell
+    % Create a COM server for the Windows Script Host Shell
     shell = actxserver('WScript.Shell');
-    
     % Load the shortcut
     shortcut = shell.CreateShortcut(version_n);
-
     % Get the target path of the shortcut
     targetPath = shortcut.TargetPath;
+    % Set the "Start In" field (Working Directory)
+    shortcut.WorkingDirectory = '';
+    % Buggy New Desktop Icon Handling
     if contains(targetPath,'MATLABWindow','IgnoreCase',1)
       disp('You have a shortcut pinned that uses the New Desktop - this may cause issues with the icons')
         % 
@@ -45,7 +46,7 @@ for L = 1:length(Shortcuts)
         shortcut.TargetPath = erase(targetPath,'Window');
         flag = 1;
         version_new_window(Win_c) = version_new;
-        version_n_window(Win_c) = string(version_n);
+        version_n_window(Win_c) = string(NewShort);
         Win_c = Win_c+1;
 
     end 
@@ -64,22 +65,20 @@ if flag == 1
         New_Icon = fullfile(pwd, 'matlab.icons',version_new_window(Count));
         % Create a COM server for the Windows Script Host Shell
         shell = actxserver('WScript.Shell');
-        
         % Load the shortcut
         shortcut = shell.CreateShortcut(version_n_window(Count));
-    
         % Get the target path of the shortcut
         targetPath = shortcut.TargetPath;  
-    
+        % Set the "Start In" field (Working Directory)
+        shortcut.WorkingDirectory = '';
         % Change the icon
         shortcut.IconLocation = New_Icon;
-        
         % Save the changes
         shortcut.Save();
-        
         % Clean up
         delete(shell);
     end
 end
+clear
 
 
